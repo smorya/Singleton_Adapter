6e4559286381a86e5146c2ced93ed2d68d856f69
smorya <maryana.skoropad@gmail.com>
2023-01-10 14:19:28 +0200

    fixing

--- src/main/java/org/example/task1/Main.java
+++ src/main/java/org/example/task1/Main.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
-        User user = new User("Nastia", "18", "anastasiyapetrovych@gmail.com");
+        User user = new User("Mariya", "36", "mariya@gmail.com");
         user.save();
-        User user2 = new User("Olia", "12", "olia@gmail.com");
+        User user2 = new User("Maksym", "36", "maksym@gmail.com");
         user2.save();
 
     }
